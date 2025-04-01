e5d37a7cedc15fb258fb29ccde5440856d6793f5
lys-0820 <silhouette223@outlook.com>
2025-04-01 18:58:58 +0200

    bug fix

--- Assets/Scripts/BalconyScene/BalconySceneController.cs
+++ Assets/Scripts/BalconyScene/BalconySceneController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class BalconySceneController : MonoBehaviour
 {
-    public Button BtHome;
+    [SerializeField] private Button BtHome;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
